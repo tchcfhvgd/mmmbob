1948a6744f4f3f4a71c9a44bb96e2dd37de0c754
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-10-05 09:25:07 +0300

    Update CoolUtil.hx

--- sourceJon/CoolUtil.hx
+++ sourceJon/CoolUtil.hx
@@ -6,7 +6,7 @@ using StringTools;
 
 class CoolUtil
 {
-	public static var difficultyArray:Array<String> = ['EASY', "NORMAL", "HARD"];
+	public static var difficultyArray:Array<String> = ['EASY', 'NORMAL', 'HARD'];
 
 	public static function difficultyString():String
 	{
@@ -21,25 +21,5 @@ class CoolUtil
 			daList[i] = daList[i].trim();
 
 		return daList;
-	}
-	
-	public static function coolStringFile(path:String):Array<String>
-		{
-			var daList:Array<String> = path.trim().split('\n');
-	
-			for (i in 0...daList.length)
-				daList[i] = daList[i].trim();
-	
-			return daList;
-		}
-
-	public static function numberArray(max:Int, ?min = 0):Array<Int>
-	{
-		var dumbArray:Array<Int> = [];
-		for (i in min...max)
-		{
-			dumbArray.push(i);
-		}
-		return dumbArray;
 	}
 }
