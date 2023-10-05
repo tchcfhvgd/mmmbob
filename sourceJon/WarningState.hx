53e725919145f618bd450ad34623b3170de79f05
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-10-05 10:05:15 +0300

    Update WarningState.hx

--- sourceJon/WarningState.hx
+++ sourceJon/WarningState.hx
@@ -1,33 +1,40 @@
 package;
-import flixel.FlxSprite;
-import flixel.sound.FlxSound;
-import flixel.util.FlxTimer;
-import flixel.*;
-import flixel.tweens.FlxEase;
-import flixel.tweens.FlxTween;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 
 class WarningState extends MusicBeatState
 {
-	var music:FlxSound;
-	var wentout:Bool = false;
-	override function create()
+	override function create():Void
 	{
 		super.create();
+
 		FlxG.sound.music.fadeIn(0.5, 0.7, 0.1);
-		var thx:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('WARNINGSCRENWARNINGSCREN', 'preload'));
-		add(thx);
+
+		add(new FlxSprite(0, 0, Paths.image('WARNINGSCRENWARNINGSCREN', 'preload')));
 	}
 
-	override function update(elapsed:Float)
+	override function update(elapsed:Float):Void
 	{
-		if (controls.ACCEPT && !wentout)
+		#if mobile
+		for (touch in FlxG.touches.list)
 		{
-			wentout = true;
+			if (touch.justPressed)
+			{
+				FlxG.sound.music.fadeIn(0.5, 0.1, 0.7);
+
+				FlxG.switchState(new ThankYouState());
+			}
+		}
+		#end
+
+		if (controls.ACCEPT)
+		{
 			FlxG.sound.music.fadeIn(0.5, 0.1, 0.7);
+
 			FlxG.switchState(new ThankYouState());
 		}
+
 		super.update(elapsed);
 	}
-	
 }
