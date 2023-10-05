e0ff85b4acff900a870ffcb3d2fab36b08602b5b
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2023-10-05 10:00:14 +0300

    Update ThankYouState.hx

--- sourceJon/ThankYouState.hx
+++ sourceJon/ThankYouState.hx
@@ -1,28 +1,31 @@
 package;
 
+import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxSprite;
-import flixel.FlxSubState;
-import flixel.text.FlxText;
-import flixel.util.FlxColor;
-import lime.app.Application;
 
 class ThankYouState extends MusicBeatState
 {
-	override function create()
+	override function create():Void
 	{
 		super.create();
-		var thx:FlxSprite = new FlxSprite(0, 0).loadGraphic(Paths.image('bob/thankers', 'shared'));
-		add(thx);
+
+		add(new FlxSprite(0, 0, Paths.image('bob/thankers', 'shared')));
+
 		FlxG.camera.fade(FlxColor.BLACK, 0.8, true);
 	}
 
-	override function update(elapsed:Float)
+	override function update(elapsed:Float):Void
 	{
+		#if mobile
+		for (touch in FlxG.touches.list)
+			if (touch.justPressed)
+				FlxG.switchState(new MainMenuState());
+		#end
+
 		if (controls.ACCEPT)
-		{
 			FlxG.switchState(new MainMenuState());
-		}
+
 		super.update(elapsed);
 	}
 }
